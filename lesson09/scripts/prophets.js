df4533a90f843d6487d143f8799f8e3144721412
minenyaraya <minenyaraya@gmail.com>
2023-11-11 14:20:56 -0600

    prophets

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,31 +1,40 @@
-const URL = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
-const cards = document.querySelector(".cards");
+const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
 
 async function getProphetData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.table(data.prophets); // temporary testing of data retreival
-  }
-  
-  getProphetData();
+  const response = await fetch(url);
+  const data = await response.json();
+  console.table(data.prophets);
+  displayProphets(data.prophets);
+}
 
-  async function getProphetData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    //console.table(data.prophets);
-    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
-  }
-  
-  getProphetData();
-  
-  const displayProphets = (prophets) => {
-    // card build code goes here
-  }
+const displayProphets = (prophets) => {
+  const cards = document.querySelector('div.cards');
 
-  const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
-  
-    });
-  }
+  prophets.forEach((prophet) => {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let portrait = document.createElement('img');
+    let dob = document.createElement('p');
+    let pob = document.createElement('p');
 
-  
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+
+    dob.textContent = `Date of birth: ${prophet.birthdate}`
+    pob.textContent = `Place of birth: ${prophet.birthplace}`
+
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.name}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    card.appendChild(h2);
+    card.appendChild(dob);
+    card.appendChild(pob);
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+  });
+}
+
+getProphetData();
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,11 @@ document.querySelector(".year").textContent = year
 let currentdate = document.lastModified;
 console.log(currentdate);
 document.querySelector(".highlight").textContent=currentdate
+
+
+
+function getHiddenDate() {
+    let dateElement = document.querySelector("#hiddenDate");
+    dateElement.value = date.toLocaleDateString("en-US", lastModifiedOptions);
+    console.log(hiddenDate.value);
+};
