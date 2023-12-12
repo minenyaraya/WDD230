5eb8b27e0536b8a4febbbad9b77c824d6e7a1d87
minenyaraya <minenyaraya@gmail.com>
2023-12-12 16:41:56 -0600

    scooters

---
+++ scoots/script/anuncio.js
@@ -0,0 +1,33 @@
+const banner = document.querySelector('#top-page-banner');
+
+let dayOfWeek = new Date().getDay();
+
+if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3|| dayOfWeek == 4 || dayOfWeek == 5 || dayOfWeek == 6 || dayOfWeek == 7) {
+    banner.style.display = 'block';
+}
+else {
+    banner.style.display = 'none';
+}
+
+// Script for closing the banner
+const closeBtn = document.querySelector('.bannerBtn');
+closeBtn.addEventListener('click', function() {
+    if (banner.style.display !== "none") {
+        banner.style.display = "none";
+    }
+})
+
+
+let date = new Date ();
+console.log(date);
+let year = date.getFullYear();
+console.log(year);
+document.querySelector(".year").textContent = year
+let currentdate = document.lastModified;
+console.log(currentdate);
+document.querySelector(".highlight").textContent=currentdate
+
+function lastUpdated() {
+    let a = document.lastModified;
+    document.getElementById("lastmodified").innerHTML = a;
+}
